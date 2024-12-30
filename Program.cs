1eb0300c36067ca8e9b8d98e611e061c9268fab9
useroyco <betgrlyn@gmail.com>
2024-12-30 07:17:16 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -58,7 +58,7 @@
                     }
                     ogrencibilgileri[i, 4] = final.ToString();
 
-                    ortalama = vize * 0.4f + final * 0.6f;
+                    ortalama = vize * 0.4 + final * 0.6f
                     ogrencibilgileri[i, 5] = ortalama.ToString();
                     toplamnot += ortalama;
 
